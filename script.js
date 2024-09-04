547dcb33a2f338df9b53b4cb5bcd93b5f6d8b81e
Anagha402 <anagharamesh18@gmail.com>
2024-09-04 21:53:49 +0530

    form

--- script.js
+++ script.js
@@ -8,44 +8,45 @@
 
 // })
 
-// tl.from("#coffeecan",{
-//     y:1000,
+
+// t1.from("#coffeecan",{
+//     y:800,
 //     duration:1,
-//     scale:2
+//     scale:0.9
 
 // })
-
 // tl.from("#coffeebean1",{
 //     y:800,
 //     duration:1
 
-// })
+// },"hi")
 // tl.from("#coffeebean2",{
 //     y:800,
 //     duration:1
 
-// })
+// },"hi")
 // tl.from("#coffeebean3",{
 //     y:800,
 //     duration:1
 
-// })
+// },"hi")
 // tl.from("#coffeebean4",{
 //     y:800,
 //     duration:1
 
-// })
+// },"hi")
 // tl.from("#coffeebean5",{
 //     y:800,
 //     duration:1
 
-// })
+// },"hi")
 // tl.from("#coffeebean6",{
 //     y:800,
 //     duration:1
 
-// })
-// about
+// },"hi")
+
+// // about
 let t2=gsap.timeline({
     scrollTrigger:{
         trigger:"#about",
@@ -88,7 +89,8 @@ let t3=gsap.timeline({
 t3.to("#coffeecan",{
     top:1660,
     left:647,
-    scale:0.8
+    scale:0.8,
+    
 
 
 },"hi")
@@ -97,33 +99,92 @@ t3.to("#coffeebean5",{
     top:1700,
     left:777,
     scale:0.7,
-    rotate:239
+    rotate:239,
+    
 
 },"hi")
 
 t3.from("#fanta",{ //coming from somewhere else to original position
     x:-300,
-    duration:1
+    
+    
 
 },"hi")
 
 t3.from("#sprite",{//coming from somewhere else to original position
     x:300,
-    duration:1
+    
 
 },"hi")
 
 
 t3.from("#bean1",{ //coming from somewhere else to original position
     x:-300,
-    duration:1,
-    rotate:200
+    
+    rotate:200,
+    
 
 },"hi")
 
 t3.from("#bean2",{//coming from somewhere else to original position
     x:300,
-    duration:1,
-    rotate:200
+    
+    rotate:200,
+    
+
+},"hi")
+
+// contact
+
+let t4=gsap.timeline({
+    scrollTrigger:{
+        trigger:"#contact",
+        scroller:"body",
+        scrub:2,
+        marker:true,
+        start:"0% 90%",
+        end:"50% 40%"
+    }
+},"hi")
+t4.to("#coffeecan",{
+        top:2485,
+        left:1032,
+        scale:0.7,
+        zIndex:2, //for making both other cans appear behind main can
+        
+    
+    },"hi")
+
+    
+t4.to("#coffeebean5",{
+    top:2483,
+    left:1119,
+    scale:0.7,
+    rotate:20,
+    
+    
+
+},"hi")
+
+
+t4.to("#fanta",{ 
+    top:2710,
+    left:840,
+    scale:0.8,
+    rotate:-20,
+    
+    
+    
+
+},"hi")
+
+t4.to("#sprite",{
+    top:2690,
+    left:1160,
+    scale:0.8,
+    rotate:20,
+    
+    
+    
 
 },"hi")
