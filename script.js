b5c1d5c950aee66eb27d94d4d2d72aa78cfb5496
Anagha402 <anagharamesh18@gmail.com>
2024-09-04 17:26:47 +0530

    second

--- script.js
+++ script.js
@@ -59,12 +59,71 @@ let t2=gsap.timeline({
 t2.to("#coffeecan",{
     top:950,
     left:130,
-    scale:0.7
+    scale:0.7,
+    
 
-})
-t2.to("#coffeebean4",{
-    top:950,
-    left:130,
-    scale:0.7
+},"hi")//hi or any other word is used as id is used to make same thing appear to two different elements at same time
+t2.to("#coffeebean5",{
+    top:990,
+    left:253,
+    scale:0.7,
+    rotate:142,
+    
+    
 
-})
+},"hi")
+
+// menu
+let t3=gsap.timeline({
+    scrollTrigger:{
+        trigger:"#menu",
+        scroller:"body",
+        scrub:2,
+        marker:true,
+        start:"0% 90%",
+        end:"50% 40%"
+    }
+},"hi")
+
+t3.to("#coffeecan",{
+    top:1660,
+    left:647,
+    scale:0.8
+
+
+},"hi")
+
+t3.to("#coffeebean5",{
+    top:1700,
+    left:777,
+    scale:0.7,
+    rotate:239
+
+},"hi")
+
+t3.from("#fanta",{ //coming from somewhere else to original position
+    x:-300,
+    duration:1
+
+},"hi")
+
+t3.from("#sprite",{//coming from somewhere else to original position
+    x:300,
+    duration:1
+
+},"hi")
+
+
+t3.from("#bean1",{ //coming from somewhere else to original position
+    x:-300,
+    duration:1,
+    rotate:200
+
+},"hi")
+
+t3.from("#bean2",{//coming from somewhere else to original position
+    x:300,
+    duration:1,
+    rotate:200
+
+},"hi")
